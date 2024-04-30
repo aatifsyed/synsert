9700b5eece5eb3fa4e5629e6c2f8b3f68ba11c7a
Aatif Syed <aatifsyedyp@gmail.com>
2024-04-30 04:57:54 +0100

    example: rpc-method-permissions

--- examples/common/mod.rs
+++ examples/common/mod.rs
@@ -1,6 +1,52 @@
-use console::{style, Style};
+use console::{style, Style, Term};
 use similar::{ChangeTag, TextDiff};
-use std::fmt;
+use std::{error::Error, fmt, fs, path::Path};
+use synsert::Editor;
+
+pub fn harness<'path, V>(
+    files: impl IntoIterator<Item = &'path Path>,
+    mut begin: impl FnMut(Editor) -> V,
+    mut end: impl FnMut(V) -> Editor,
+) -> Result<(), Box<dyn Error>>
+where
+    for<'ast> V: syn::visit::Visit<'ast>,
+{
+    let mut edited_files = 0;
+    let mut edited_sites = 0;
+    for path in files {
+        print!("{}... ", Style::new().apply_to(path.display()).dim());
+        let before = fs::read_to_string(path)?;
+        let Ok(ast) = syn::parse_file(&before) else {
+            println!("skipped (failed to parse file)");
+            continue;
+        };
+        let mut visitor = begin(Editor::new(&before));
+        visitor.visit_file(&ast);
+        let editor = end(visitor);
+        match editor.len() {
+            0 => println!("no edits."),
+            n => {
+                println!("{} edits to apply!", n);
+
+                let after = editor.finish();
+                print_diff(&before, &after);
+
+                if dialoguer::Confirm::new()
+                    .with_prompt("save the edited file? ")
+                    .default(true)
+                    .interact()?
+                {
+                    fs::write(path, after)?;
+                    edited_files += 1;
+                    edited_sites += n;
+                }
+            }
+        }
+    }
+    println!("edited {} sites in {} files", edited_sites, edited_files);
+    Term::stderr().show_cursor()?; // clean up after rustyline on Ctrl+C
+    Ok(())
+}
 
 // https://github.com/mitsuhiko/similar/blob/de455873dab514082bf6e7bb5f0029837fe280d5/examples/terminal-inline.rs
 pub fn print_diff(old: &str, new: &str) {
